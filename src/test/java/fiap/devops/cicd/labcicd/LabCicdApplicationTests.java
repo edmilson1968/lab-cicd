ffc25fc39a25659178933ae113e3a1c105cc4874
Edmilson Figueiredo <edmilson.figueiredo@gmail.com>
2020-02-08 12:30:06 -0500

    acerto nos imports

--- src/test/java/fiap/devops/cicd/labcicd/LabCicdApplicationTests.java
+++ src/test/java/fiap/devops/cicd/labcicd/LabCicdApplicationTests.java
@@ -2,6 +2,7 @@ package fiap.devops.cicd.labcicd;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class LabCicdApplicationTests {
